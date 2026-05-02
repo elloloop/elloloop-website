c152549fe97aa63b77e47e86f16df19b570b89f1
Arun Saragadam <arun88m@gmail.com>
2026-05-02 23:32:25 +0100

    fix: use refraction-ui astro package instead of react

--- src/components/ThemeProvider.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { ThemeProvider as RefractionThemeProvider } from '@refraction-ui/react';
-
-export default function ThemeProvider({ children }: { children: React.ReactNode }) {
-  return <RefractionThemeProvider>{children}</RefractionThemeProvider>;
-}
--- src/components/ThemeToggle.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { ThemeToggle as RefractionThemeToggle } from '@refraction-ui/react';
-
-export default function ThemeToggle() {
-  return <RefractionThemeToggle variant="dropdown" />;
-}
